b8398976339139cb88d60765217d588ae4ae330f
KVARA7 <abdeljalil0aeb@gmail.com>
2023-10-20 14:54:33 +0100

    ☄️

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { useState } from 'react'
-import { NFTCard } from './components/nftCard'
+import { NFTCard } from '../components/nftCard'
 
 const Home = () => {
   const [wallet, setWalletAddress] = useState('')
